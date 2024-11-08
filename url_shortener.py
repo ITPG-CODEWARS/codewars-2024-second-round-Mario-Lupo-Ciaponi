b80a583fc0b518fd0d137dc497f1705f0d171178
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2024-11-08 14:50:34 +0200

    Added 'Preview' button, and added docs to functions.

--- url_shortener.py
+++ url_shortener.py
@@ -3,6 +3,7 @@ from math import floor
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.dialogs import Messagebox
+from ttkbootstrap.window import Toplevel
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import mysql.connector
@@ -12,6 +13,7 @@ import pyperclip
 import pyqrcode
 import png
 import re
+
 
 # Flask app for URL redirection
 app = Flask(__name__)
@@ -23,6 +25,9 @@ REGEX_FOR_VALIDATING_URL = (r"(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/
 
 
 def connect_db():
+    """
+    Connects to the database.
+    """
     conn = mysql.connector.connect(
         database="url_shortener",
         user="root",
@@ -34,13 +39,16 @@ def connect_db():
 
 
 def hash_url(original_url, length_of_url):
-    # Generate a hash-based short URL if no custom code is provided
-    return hashlib.md5(original_url.encode()).hexdigest()[:length_of_url]
+    """
+    Generate a hash-based short URL if no custom code is provided.
+    """
+    return hashlib.md5(original_url.encode()).hexdigest()[:length_of_url]  # The value of the slider (length_of_url)
 
 
 def save_url(original_url, short_url):
     conn = connect_db()
     cursor = conn.cursor()
+
     try:
         # Check if the short URL already exists
         cursor.execute("SELECT * FROM urls WHERE short_url = %s", (short_url,))
@@ -48,7 +56,8 @@ def save_url(original_url, short_url):
         if result:
             return result[1]
         else:
-            cursor.execute("INSERT INTO urls (long_url, short_url) VALUES (%s, %s)", (original_url, short_url))
+            cursor.execute("INSERT INTO urls (long_url, short_url, date_of_creation) VALUES (%s, %s, NOW())",
+                           (original_url, short_url))
             conn.commit()
             return short_url
     except Exception as e:
@@ -61,6 +70,7 @@ def save_url(original_url, short_url):
 def get_long_url(short_url):
     conn = connect_db()
     cursor = conn.cursor()
+
     try:
         cursor.execute("SELECT * FROM urls WHERE short_url = %s", (short_url,))
         result = cursor.fetchone()
@@ -85,7 +95,9 @@ def shorten_url(original_url, length_of_url, custom_code=None):
 def redirect_to_url(short_code):
     if not short_code:
         return "Short code not provided!", 400
+
     original_url = get_long_url(short_code)
+
     if original_url:
         return redirect(original_url)
     return 'Shortened URL not found!', 404
@@ -95,8 +107,11 @@ def run_flask():
     app.run(debug=False, use_reloader=False, port=9000)
 
 
-# Tkinter GUI
+# Ttkbootstrap GUI
 def create_gui():
+    """
+    Creates a GUI for the app and inside of it, there a some nested important functions.
+    """
     window = ttk.Window(themename="superhero")
     window.title("URL Shortener")
     window.geometry("500x383")
@@ -110,16 +125,49 @@ def create_gui():
     custom_code_entry.pack(pady=5)
 
     def slider(e):
+        """
+        Changes the value of length_label
+        """
         length_label.config(text=f"{floor(length_scale.get())}")
 
     def copy_to_clipboard():
+        """
+        It copies the shortened URL to the clipboard.
+        """
         if generated_short_url:
             pyperclip.copy(generated_short_url)
             Messagebox.ok("Copied", "Short URL copied to clipboard!")
         else:
             Messagebox.ok("Copy Error", "No URL to copy. Please shorten a URL first.")
 
+    def show_urls():
+        """
+        It shows all the URLS in the database.
+        """
+        second_window = Toplevel()
+        second_window.title("Preview of URLS")
+
+        connection = connect_db()
+
+        sql_select_query = "SELECT * FROM urls"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_query)
+
+        records = cursor.fetchall()
+
+        ttk.Label(second_window, text="URLS:", font=("Helvetica", 20, "bold"), bootstyle=DANGER).pack(pady=17)
+
+        for row in records:
+            ttk.Label(second_window, text=f"Original URl: {row[1]} | "
+                                          f"Shortened URL: http://127.0.0.1:9000/{row[2]} | "
+                                          f"Date of creation: {row[3]}",
+                      font=("Helvetica", 13), bootstyle=PRIMARY).pack(pady=10, padx=25)
+
     def create_qr_code(short_url):
+        """
+        Firstly, it asks where do you want to save it(save as),
+        then it generates a qr code at the bottom of the app
+        """
         input_path = filedialog.asksaveasfilename(title="Save Image",
                                                   filetypes=(("PNG File", ".png"), ("All Files", "*.*")))
         if input_path:
@@ -132,9 +180,12 @@ def create_gui():
             global get_image
             get_image = ImageTk.PhotoImage(Image.open(input_path))
             qr_code_label.config(image=get_image)
-            window.geometry("500x600")
+            window.geometry("500x750")
 
     def on_shorten_button_click():
+        """
+        Generates a short URL after checking if the URL is valid.
+        """
         original_url = url_entry.get()
 
         if re.search(REGEX_FOR_VALIDATING_URL, original_url):  # Check if the URL given is valid
@@ -165,19 +216,23 @@ def create_gui():
     length_scale = ttk.Scale(window, length=50, from_=5, to=10, value=5, bootstyle=INFO, command=slider)
     length_scale.pack()
 
-    length_label = ttk.Label(window, text=f"{length_scale.get()}")
+    length_label = ttk.Label(window, text=f"{length_scale.get()}")  # To show the value that the slider is currently on.
     length_label.pack()
 
     shorten_button = ttk.Button(window, text="Shorten URL", command=on_shorten_button_click)
     shorten_button.pack(pady=20)
 
     global short_url_label, generated_short_url
+
     generated_short_url = ""
     short_url_label = ttk.Label(window, text="Short URL: 'No URL generated'")
     short_url_label.pack(pady=10)
 
     copy_url_button = ttk.Button(window, text="Copy URL", bootstyle=INFO, command=copy_to_clipboard)
     copy_url_button.pack()
+
+    preview_button = ttk.Button(window, text="Preview", bootstyle=INFO, command=show_urls)
+    preview_button.pack(pady=20)
 
     qr_code_label = ttk.Label(window, text="")
     qr_code_label.pack(pady=20)
